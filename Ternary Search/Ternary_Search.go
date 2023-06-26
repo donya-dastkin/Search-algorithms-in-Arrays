4c410f5be3ad632c8e0320b2bb0a6917d81058a3
Donya Dastkin <donya.dastkin583@gmail.com>
2023-06-26 23:55:04 +0330

    Complete the ternary search file

---
+++ Ternary Search/Ternary_Search.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	target := 6
+	Result := TernarySearch(arr, target)
+	fmt.Println("Result :", Result)
+}
+
+func TernarySearch(arr [10]int, target int) int {
+	var left int = 0
+	var right int = len(arr) - 1
+	mid1 := left + ((right - left) / 3)
+	mid2 := right - ((right - left) / 3)
+	for left <= right {
+		if arr[mid1] == target {
+			return mid1
+		}
+		if arr[mid2] == target {
+			return mid2
+		}
+		if target < arr[mid1] {
+			right = mid1 - 1
+		} else if target > arr[mid2] {
+			left = mid2 + 1
+		} else {
+			left = mid1 + 1
+			right = mid2 - 1
+		}
+	}
+	return -1
+}
